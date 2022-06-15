f377667149ae9218c27b82429ef9592770d72364
Dan Krupsky <dan.krupsky@gmail.com>
2022-06-15 19:31:37 +0300

    En-Ru Inverser is done

--- tools/layout-inverter/script.js
+++ tools/layout-inverter/script.js
@@ -1,20 +1,20 @@
 const inInput = document.getElementById("in");
 const outInput = document.getElementById("out");
 
-inInput.addEventListener("paste", invert);
+inInput.addEventListener("input", invert);
 
+// minor nuance (bug): . exists in both layout, / (ru) -> . -> ю
 const keysArray = '~!@#$%^&QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?`qwertyuiop[]asdfghjkl;\'\\zxcvbnm,./Ё!"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,ёйцукенгшщзхъфывапролджэ\\ячсмитьбю.';
-const length = keysArray.length;
+const keysLength = keysArray.length;
 
 function invert(e) {
     outInput.value = "";
-    console.log(navigator.clipboard.readText());
     for (let i of inInput.value) {
         let keyIndex = keysArray.indexOf(i);
-        if (keyIndex) {
-            outInput.value += keysArray[((keyIndex + (length / 2)) % length)];
+        if (keyIndex != -1) {
+            outInput.value += keysArray[((keyIndex + (keysLength / 2)) % keysLength)];
         } else {
-            outInput.value += e.key;
+            outInput.value += i;
         }
     }  
 }
