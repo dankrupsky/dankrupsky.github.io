ff8e53909ffe1a1771558c91234038fb0ed9a1cc
Dan Krupsky <dan.krupsky@gmail.com>
2022-06-18 14:43:43 +0300

    figure rendering

--- games/tetris/script.js
+++ games/tetris/script.js
@@ -35,8 +35,75 @@ const rightZigZag = [
 const triThing = [
     [fEmpty, fBlock, fEmpty],
     [fEmpty, fBlock, fBlock],  
-]
+];
 
+const leftLThing = [
+    [fBlock, fEmpty],
+    [fBlock, fEmpty],
+    [fBlock, fBlock],
+];
+
+const rightLThing = [
+    [fEmpty, fBlock],
+    [fEmpty, fBlock],
+    [fBlock, fBlock], 
+];
+
+const elCollection = [line, square, leftZigZag, rightZigZag, triThing, leftLThing, rightLThing];
+
+let isPlaying = false;
+let pos = {
+    x: 5,
+    y: 5
+};
+let element = leftZigZag;
+let score = 0;
+let highScore = 0;
+
+
+function render(container) {
+    for (let i = fieldHeight; i > -1; i--) {
+        for(let j = 0; j < fieldWidth; j++) {
+            container.textContent += gameField[i][j];
+        }
+        container.textContent += '\r\n';
+    }
+}
+
+// To Do - consistent coordinates approach
+function update() {
+    for (let i = pos.y; i > pos.y - element.length; i--) {
+        for (let j = pos.x; j < pos.x + element[0].length; j++) {
+            console.log(i, j)
+            gameField[i][j] = element[pos.y - i][j - pos.x];
+        }
+    }
+}
+
+function collisionCheck(direction) {
+    // 0 - finalize, 1 - allow, -1 - disallow
+}
+
+function move(e) {
+    // console.log(e);
+    switch (e) {
+        case "ArrowRight":
+            console.log(e);
+            break;
+        case "ArrowLeft":
+            console.log(e);
+            break;
+        case "ArrowUp":
+            console.log(e);
+            break;
+        case "ArrowDown":
+            console.log(e);
+            break;
+        case " ":
+            console.log(e);
+            break;
+    }
+}
 
 function init() {
     for (let i = 0; i < fieldHeight + spawnHeight; i++) {
@@ -46,21 +113,26 @@ function init() {
         }
         gameField.push(newFieldLine);
     }
+
+    // Controls
+    document.addEventListener('keydown', function(event) {
+        move(event.key);
+    });
 }
 
-// To do later
-function render(container) {
-    
-    for (let i = 9; i > -1; i--) {
-        for(let j = 9; j > -1; j--) {
-            console.log("smtg");
-            container.textContent += gameField[i][j];
-        }
-        container.textContent += '\r\n';
-    }
+function start() {
+    let score = 0;
+
+    let rnd = Math.floor(Math.random() * elCollection.length)
+    let element = elCollection[rnd];
+    let pos = 21 + element.length;
 }
+
+
+
 
 
 
 init();
+update();
 render(gameFieldDiv);
