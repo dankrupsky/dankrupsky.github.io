af2250eba017d848199fc0a74b971dacf4561482
Dan Krupsky <dan.krupsky@gmail.com>
2022-06-28 19:26:53 +0300

    1 field, kg2pound, working

--- tools/units-converter/script.js
+++ tools/units-converter/script.js
@@ -1,1 +1,21 @@
-alert("hw!");
+const initValue = document.getElementById("initial-value");
+const containerDiv = document.getElementById("container");
+
+initValue.addEventListener("input", checkInput);
+initValue.addEventListener("input", update);
+
+function checkInput() {
+    initValue.value = initValue.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');
+}
+
+// 1 unit for test, kg to pounds
+const kg = document.createElement('input');
+const kg2pC = 2.20462;
+containerDiv.appendChild(kg);
+
+function update() {
+    kg.value = initValue.value * kg2pC;
+}
+
+
+
