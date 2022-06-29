029daf4a2fd27d36aaf5147028132c95e19f7732
Dan Krupsky <dan.krupsky@gmail.com>
2022-06-29 15:45:28 +0300

    iteration through data object

--- tools/units-converter/script.js
+++ tools/units-converter/script.js
@@ -8,14 +8,47 @@ function checkInput() {
     initValue.value = initValue.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');
 }
 
-// 1 unit for test, kg to pounds
-const kg = document.createElement('input');
-const kg2pC = 2.20462;
-containerDiv.appendChild(kg);
+const coefficients = {
+    1: {
+        value1: "kg",
+        value2: "pounds",
+        c: 2.20462,
+    }
+};
 
-function update() {
-    kg.value = initValue.value * kg2pC;
+function init() {
+    for (let item in coefficients) {
+        // straight
+        let t = document.createElement('p');
+        let v = document.createElement('input');
+        t.textContent = coefficients[item]["value1"] + "2" + coefficients[item]["value2"];
+        v.setAttribute("id", item);
+        containerDiv.appendChild(t);
+        containerDiv.appendChild(v);
+        // reverse
+        t = document.createElement('p');
+        v = document.createElement('input');
+        t.textContent = coefficients[item]["value2"] + "2" + coefficients[item]["value1"];
+        v.setAttribute("id", item + "r");
+        containerDiv.appendChild(t);
+        containerDiv.appendChild(v);
+    }
 }
 
+
+
+function update() {
+    for (let item in coefficients) {
+        // straight
+        let v = document.getElementById(item);
+        v.value = initValue.value * coefficients[item]["c"];
+        // reverse
+        v = document.getElementById(item + "r");
+        v.value = initValue.value / coefficients[item]["c"];
+    }
+}
+
+init();
+update();
 
 
