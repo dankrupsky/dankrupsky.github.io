25288a19f6e36415f8f77c9c3349b1afcb974117
Dan Krupsky <dan.krupsky@gmail.com>
2022-07-19 17:12:05 +0300

    speed update encaps, speed variation

--- misc/floating-color/script.js
+++ misc/floating-color/script.js
@@ -1,12 +1,11 @@
 let R = Math.floor(Math.random() * 255);
 let G = Math.floor(Math.random() * 255);
 let B = Math.floor(Math.random() * 255);
-const speedSpread = 4;
-// range [1, speedSpread]
-let Rrate = Math.floor(Math.random() * speedSpread) + 1;
-let Grate = Math.floor(Math.random() * speedSpread) + 1;
-let Brate = Math.floor(Math.random() * speedSpread) + 1;
-
+const minSpeed = 2;
+const maxSpeed = 5;
+let Rrate = getNewRate();
+let Grate = getNewRate();
+let Brate = getNewRate();
 
 
 function setBackgroundColor() {
@@ -14,23 +13,35 @@ function setBackgroundColor() {
 }
 
 
+function getNewRate(currentRate = -1) {
+    // range [minSpeed, maxSpeed]
+    // + sign by default, invert sign if argument is used
+    return -Math.sign(currentRate) * (Math.floor(Math.random() * maxSpeed) + minSpeed);
+}
+
+
 function update() {
     R = R + Rrate;
     if (R > 255 || R < 0) {
-        Rrate = -Rrate;
-        R = R + Rrate;
+        R = R - Rrate;
+        Rrate = getNewRate(Rrate);
+        
     }
+
     G = G + Grate;
     if (G > 255 || G < 0) {
-        Grate = -Grate;
-        G = G + Grate;
+        G = G - Grate;
+        Grate = getNewRate(Grate);
 
     }
+
     B = B + Brate;
     if (B > 255 || B < 0) {
-        Brate = -Brate;
-        B = B + Brate;
+        B = B - Brate;
+        Brate = getNewRate(Brate)
     }
+
+    console.log(R,G,B);
     setBackgroundColor();
 }
 
