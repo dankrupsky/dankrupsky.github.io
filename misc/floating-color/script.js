9cc9ee4024fd2411082049a3fbe8b2a703ae0de4
Dan Krupsky <dan.krupsky@gmail.com>
2022-07-19 16:52:57 +0300

    zero rate bugfix, rgb range bugfix

--- misc/floating-color/script.js
+++ misc/floating-color/script.js
@@ -2,9 +2,10 @@ let R = Math.floor(Math.random() * 255);
 let G = Math.floor(Math.random() * 255);
 let B = Math.floor(Math.random() * 255);
 const speedSpread = 4;
-let Rrate = Math.floor(Math.random() * speedSpread);
-let Grate = Math.floor(Math.random() * speedSpread);
-let Brate = Math.floor(Math.random() * speedSpread);
+// range [1, speedSpread]
+let Rrate = Math.floor(Math.random() * speedSpread) + 1;
+let Grate = Math.floor(Math.random() * speedSpread) + 1;
+let Brate = Math.floor(Math.random() * speedSpread) + 1;
 
 
 
@@ -15,15 +16,23 @@ function setBackgroundColor() {
 
 function update() {
     R = R + Rrate;
-    if (R > 255 || R < 0) Rrate = -Rrate;
+    if (R > 255 || R < 0) {
+        Rrate = -Rrate;
+        R = R + Rrate;
+    }
     G = G + Grate;
-    if (G > 255 || G < 0) Grate = -Grate;
+    if (G > 255 || G < 0) {
+        Grate = -Grate;
+        G = G + Grate;
+
+    }
     B = B + Brate;
-    if (B > 255 || B < 0) Brate = -Brate;
-    
+    if (B > 255 || B < 0) {
+        Brate = -Brate;
+        B = B + Brate;
+    }
     setBackgroundColor();
 }
-
 
 
 setInterval(update, 50);
