fcc4ed520cc62718fb6657519f00f200ceb6a63f
Dan Krupsky <dan.krupsky@gmail.com>
2022-07-31 07:39:41 +0300

    encapsulated in class
    constructor init fixed to proper range

--- misc/floating-color/script.js
+++ misc/floating-color/script.js
@@ -1,49 +1,56 @@
-let R = Math.floor(Math.random() * 255);
-let G = Math.floor(Math.random() * 255);
-let B = Math.floor(Math.random() * 255);
-const minSpeed = 2;
-const maxSpeed = 5;
-let Rrate = getNewRate();
-let Grate = getNewRate();
-let Brate = getNewRate();
+class FloatingColor {
+    constructor(min = 1, max = 3) {
+        this.R = Math.floor(Math.random() * 256);
+        this.G = Math.floor(Math.random() * 256);
+        this.B = Math.floor(Math.random() * 256);
+        this.minSpeed = min;
+        this.maxSpeed = max;
+        this.Rrate = this.getNewRate();
+        this.Grate = this.getNewRate();
+        this.Brate = this.getNewRate();
+    }
+
+    getNewRate(currentRate = -1) {
+        // range [minSpeed, maxSpeed]
+        // + sign by default, invert sign if argument is used
+        const rangeLength = this.maxSpeed - this.minSpeed + 1; 
+        return -Math.sign(currentRate) * (Math.floor(Math.random() * rangeLength) + this.minSpeed);
+    }
+
+    update() {
+        this.R = this.R + this.Rrate;
+        if (this.R > 255 || this.R < 0) {
+            this.R = this.R - this.Rrate;
+            this.Rrate = this.getNewRate(this.Rrate);
+            
+        }
+    
+        this.G = this.G + this.Grate;
+        if (this.G > 255 || this.G < 0) {
+            this.G = this.G - this.Grate;
+            this.Grate = this.getNewRate(this.Grate);
+    
+        }
+    
+        this.B = this.B + this.Brate;
+        if (this.B > 255 || this.B < 0) {
+            this.B = this.B - this.Brate;
+            this.Brate = this.getNewRate(this.Brate)
+        }
+    }
+}
 
 
-function setBackgroundColor() {
+function setBackgroundColor(R, G, B) {
     document.body.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
 }
 
-
-function getNewRate(currentRate = -1) {
-    // range [minSpeed, maxSpeed]
-    // + sign by default, invert sign if argument is used
-    const rangeLength = maxSpeed - minSpeed + 1; 
-    return -Math.sign(currentRate) * (Math.floor(Math.random() * rangeLength) + minSpeed);
-}
-
-
 function update() {
-    R = R + Rrate;
-    if (R > 255 || R < 0) {
-        R = R - Rrate;
-        Rrate = getNewRate(Rrate);
-        
-    }
-
-    G = G + Grate;
-    if (G > 255 || G < 0) {
-        G = G - Grate;
-        Grate = getNewRate(Grate);
-
-    }
-
-    B = B + Brate;
-    if (B > 255 || B < 0) {
-        B = B - Brate;
-        Brate = getNewRate(Brate)
-    }
-
-    setBackgroundColor();
+    rgb.update();
+    setBackgroundColor(rgb.R, rgb.G, rgb.B);
 }
 
 
-setInterval(update, 50);
+let rgb = new FloatingColor(1, 3);
+setBackgroundColor();
+setInterval(update, 40);
