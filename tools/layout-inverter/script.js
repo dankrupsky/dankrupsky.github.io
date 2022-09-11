58ccbcb34ccd671a0bacf37820b154da1f55b6e7
Dan Krupsky <dan.krupsky@gmail.com>
2022-09-11 08:44:20 +0300

    copy-paste buttons

--- tools/layout-inverter/script.js
+++ tools/layout-inverter/script.js
@@ -1,7 +1,11 @@
 const inInput = document.getElementById("in");
 const outInput = document.getElementById("out");
+const copyBtn = document.getElementById("copy-btn");
+const pasteBtn = document.getElementById("paste-btn");
 
 inInput.addEventListener("input", invert);
+copyBtn.addEventListener("click", copyTextToClipboard);
+pasteBtn.addEventListener("click", pasteTextToClipboard);
 
 // minor nuance (bug): . exists in both layout, / (ru) -> . -> ю
 const keysArray = '~!@#$%^&QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?`qwertyuiop[]asdfghjkl;\'\\zxcvbnm,./Ё!"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,ёйцукенгшщзхъфывапролджэ\\ячсмитьбю.';
@@ -17,4 +21,16 @@ function invert(e) {
             outInput.value += i;
         }
     }  
+}
+
+function copyTextToClipboard() {
+    navigator.clipboard.writeText(outInput.value);
+}
+
+function pasteTextToClipboard() {
+    navigator.clipboard.readText().then(e => {
+        inInput.value = e;
+        invert();
+    });
+    
 }
